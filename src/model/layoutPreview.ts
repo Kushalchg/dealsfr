0721c1164594237fd34e81ceb51e47a1fa26363d
Arpit Sharma <frozenboyarp@gmail.com>
2025-08-08 11:29:47 +0545

    feat: manage store layouts via API

--- src/model/layoutPreview.ts
+++ src/model/layoutPreview.ts
@@ -1,6 +1,7 @@
 type ValuesArray = [number, number, number] | [number, number, number, number];
 
 export default interface LayoutPreview {
+  id?: number;
   name: string;
   values: ValuesArray;
   store: number;
